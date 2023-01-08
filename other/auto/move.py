0f0a6471395597fefb8ac52106c12915bd205a51
funkyman228 <avgamer247@yahoo.com>
2023-01-07 22:11:35 -0800

    testing

--- other/auto/move.py
+++ other/auto/move.py
@@ -2,7 +2,6 @@ import pyautogui
 
 text = "Work on what would become the Antonov An 225 would begin in 1984 with a from the Soviet government for a large airlifter as a replacement for the Myasishchev VM-T."
 test = 38
-a = 1
 
 pyautogui.moveTo(-745, 950)
 pyautogui.click()
